a6ea683c437c1559ae55f823da5c726df608192b
Keevan Dance <keevandance@nau.edu>
2016-10-17 21:35:46 -0700

    Added event for submit

--- app/src/main/java/com/urch/dance/anagram/Game.java
+++ app/src/main/java/com/urch/dance/anagram/Game.java
@@ -4,20 +4,31 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class Game extends AppCompatActivity {
+import static com.urch.dance.anagram.R.string.found;
+import static com.urch.dance.anagram.R.string.letters;
 
+public class Game extends AppCompatActivity {
+    ArrayList<String> foundList;
+    ArrayAdapter<String> foundAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
         LinearLayout ll = (LinearLayout)findViewById(R.id.word_container);
-
+        foundList = new ArrayList<String>();
+        foundAdapter = new ArrayAdapter<String>(this, R.layout.list_item, foundList);
+        ListView lv = (ListView)findViewById(R.id.listview);
+        lv.setAdapter(foundAdapter);
+        Button sub = (Button)findViewById(R.id.submit);
+        sub.setOnClickListener(new submitClick());
         ArrayList<Button> letters = new ArrayList<Button>();
         for(int i=0; i<5; i++){
             Button b = new Button(this);
@@ -48,6 +59,26 @@ public class Game extends AppCompatActivity {
 
     }
 
+    public class submitClick implements View.OnClickListener {
+
+        public void onClick(View view) {
+            int i = view.getId();
+
+            Button submit_word = (Button) findViewById(i);
+            LinearLayout fc = (LinearLayout)findViewById(R.id.found_container);
+            String word = "";
+            for (int j = 0; j < fc.getChildCount(); j++) {
+                View v = fc.getChildAt(i);
+                if (v instanceof Button) {
+                    word += ((Button) v).getText().toString();
+                }
+            }
+            //validation
+            foundList.add(word);
+            foundAdapter.notifyDataSetChanged();
+        }
+    }
+
     public class foundClick implements View.OnClickListener {
 
         public void onClick(View view){
@@ -58,7 +89,6 @@ public class Game extends AppCompatActivity {
             fc.removeView(b);
             wc.addView(b);
             b.setOnClickListener(new letterClick());
-
         }
 
     }
