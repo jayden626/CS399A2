9cb078a9d8e973588bb21527717aa9220c6e566c
jayden626 <jayden_626@hotmail.com>
2016-10-18 22:56:49 -0700

    Submit button now adds words to list and returns letters to the unclicked area

--- app/src/main/java/com/urch/dance/anagram/Game.java
+++ app/src/main/java/com/urch/dance/anagram/Game.java
@@ -1,8 +1,11 @@
 package com.urch.dance.anagram;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -10,6 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.urch.dance.anagram.R.string.found;
 import static com.urch.dance.anagram.R.string.letters;
@@ -17,6 +21,7 @@ import static com.urch.dance.anagram.R.string.letters;
 public class Game extends AppCompatActivity {
     ArrayList<String> foundList;
     ArrayAdapter<String> foundAdapter;
+    ArrayList<Button> clickedButtons;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,7 @@ public class Game extends AppCompatActivity {
 
         LinearLayout ll = (LinearLayout)findViewById(R.id.word_container);
         foundList = new ArrayList<String>();
+        clickedButtons  = new ArrayList<Button>();
         foundAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foundList);
         ListView lv = (ListView)findViewById(R.id.listview);
         lv.setAdapter(foundAdapter);
@@ -42,6 +48,12 @@ public class Game extends AppCompatActivity {
             b.setOnClickListener(new letterClick());
             ll.addView(b);
         }
+
+    }
+
+    public void about(View view) {
+        Intent intent = new Intent(this, About.class);
+        startActivity(intent);
     }
 
     public class letterClick implements View.OnClickListener {
@@ -49,6 +61,7 @@ public class Game extends AppCompatActivity {
         public void onClick(View view){
             int i = view.getId();
             Button b = (Button) view.findViewById(i);
+            clickedButtons.add(b);
             LinearLayout wc = (LinearLayout)findViewById(R.id.word_container);
             LinearLayout fc = (LinearLayout)findViewById(R.id.found_container);
             wc.removeView(b);
@@ -62,20 +75,38 @@ public class Game extends AppCompatActivity {
     public class submitClick implements View.OnClickListener {
 
         public void onClick(View view) {
-            int i = view.getId();
+            if (clickedButtons.size() > 0) {
+                int i = view.getId();
 
-            Button submit_word = (Button) findViewById(i);
-            LinearLayout fc = (LinearLayout)findViewById(R.id.found_container);
-            String word = "";
-            for (int j = 0; j < fc.getChildCount(); j++) {
-                View v = fc.getChildAt(i);
-                if (v instanceof Button) {
-                    word += ((Button) v).getText().toString();
+                Button submit_word = (Button) findViewById(i);
+                LinearLayout fc = (LinearLayout) findViewById(R.id.found_container);
+                LinearLayout wc = (LinearLayout) findViewById(R.id.word_container);
+                ListView lv = (ListView) findViewById(R.id.listview);
+                String word = "";
+
+                for (Button b : clickedButtons) {
+                    word += b.getText().toString();
+                    fc.removeView(b);
+                    wc.addView(b);
+                    b.setOnClickListener(new letterClick());
                 }
+                clickedButtons.clear();
+                /*for (int j = 0; j < fc.getChildCount(); j++) {
+                    View v = fc.getChildAt(i);
+                    if (v.getClass().isInstance(submit_word)) {
+                        Log.i("b", "isbutton");
+                        word += ((Button) v).getText().toString();
+                    }
+                    Log.i("b", "isbutton");
+                }*/
+                //validation
+                //Check if word already on list
+                //then check if is real word
+                Log.i("word", "word: " + word);
+                foundList.add(word);
+                foundAdapter.notifyDataSetChanged();
+                lv.setSelection(foundAdapter.getCount() - 1);
             }
-            //validation
-            foundList.add(word);
-            foundAdapter.notifyDataSetChanged();
         }
     }
 
@@ -84,6 +115,7 @@ public class Game extends AppCompatActivity {
         public void onClick(View view){
             int i = view.getId();
             Button b = (Button) view.findViewById(i);
+            clickedButtons.remove(b);
             LinearLayout wc = (LinearLayout)findViewById(R.id.word_container);
             LinearLayout fc = (LinearLayout)findViewById(R.id.found_container);
             fc.removeView(b);
