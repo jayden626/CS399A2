5596f51ecce8d74bcda067624e0e7594b9ee7d6d
jayden626 <jayden_626@hotmail.com>
2016-10-19 14:55:17 -0700

    Added timer and linking to results screen

--- app/src/main/java/com/urch/dance/anagram/Game.java
+++ app/src/main/java/com/urch/dance/anagram/Game.java
@@ -1,6 +1,8 @@
 package com.urch.dance.anagram;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.CountDownTimer;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,9 +13,17 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static com.urch.dance.anagram.R.string.found;
 import static com.urch.dance.anagram.R.string.letters;
@@ -22,33 +32,59 @@ public class Game extends AppCompatActivity {
     ArrayList<String> foundList;
     ArrayAdapter<String> foundAdapter;
     ArrayList<Button> clickedButtons;
+    TextView timerText;
+    CountDownTimer timer;
+    int timeRemaining;
+    int incorrect;
+    //CountdownActivity count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        LinearLayout ll = (LinearLayout)findViewById(R.id.word_container);
+        LinearLayout ll = (LinearLayout) findViewById(R.id.word_container);
         foundList = new ArrayList<String>();
-        clickedButtons  = new ArrayList<Button>();
+        clickedButtons = new ArrayList<Button>();
         foundAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foundList);
-        ListView lv = (ListView)findViewById(R.id.listview);
+        ListView lv = (ListView) findViewById(R.id.listview);
         lv.setAdapter(foundAdapter);
-        Button sub = (Button)findViewById(R.id.submit);
+        Button sub = (Button) findViewById(R.id.submit);
         sub.setOnClickListener(new submitClick());
         ArrayList<Button> letters = new ArrayList<Button>();
-        for(int i=0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             Button b = new Button(this);
             b.setText(String.valueOf(i));
             b.setLayoutParams(new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT));
             b.setId(i);
-           // b.setMaxWidth(1);
-           // b.setMaxHeight(1);
-           // b.setMinimumHeight(0);
-           // b.setMinimumWidth(0);
+            // b.setMaxWidth(1);
+            // b.setMaxHeight(1);
+            // b.setMinimumHeight(0);
+            // b.setMinimumWidth(0);
             b.setOnClickListener(new letterClick());
             ll.addView(b);
         }
 
+        incorrect = 0;
+
+        timerText = (TextView) findViewById(R.id.timer);
+        timeRemaining = 3000;
+        timer = new CountDownTimer(timeRemaining, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timerText.setText(millisUntilFinished / 1000 + "s");
+            }
+
+            public void onFinish() {
+                Intent intent = new Intent(Game.this, Results.class);
+                intent.putExtra("correct", String.valueOf(foundList.size()));
+                intent.putExtra("incorrect", String.valueOf(incorrect));
+                intent.putExtra("total", String.valueOf(foundList.size() + incorrect));
+                startActivity(intent);
+            }
+
+        }.start();
+       // count = new CountdownActivity();
     }
 
     public void about(View view) {
@@ -56,14 +92,15 @@ public class Game extends AppCompatActivity {
         startActivity(intent);
     }
 
+
     public class letterClick implements View.OnClickListener {
 
-        public void onClick(View view){
+        public void onClick(View view) {
             int i = view.getId();
             Button b = (Button) view.findViewById(i);
             clickedButtons.add(b);
-            LinearLayout wc = (LinearLayout)findViewById(R.id.word_container);
-            LinearLayout fc = (LinearLayout)findViewById(R.id.found_container);
+            LinearLayout wc = (LinearLayout) findViewById(R.id.word_container);
+            LinearLayout fc = (LinearLayout) findViewById(R.id.found_container);
             wc.removeView(b);
             fc.addView(b);
             b.setOnClickListener(new foundClick());
@@ -100,24 +137,44 @@ public class Game extends AppCompatActivity {
                     Log.i("b", "isbutton");
                 }*/
                 //validation
-                //Check if word already on list
-                //then check if is real word
-                Log.i("word", "word: " + word);
+                /* if(word is valid){*/
+                timer.cancel();
+                timeRemaining += 5000;
+                timer = new CountDownTimer(timeRemaining, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        timerText.setText(millisUntilFinished / 1000 + "s");
+                    }
+
+                    public void onFinish() {
+                        Intent intent = new Intent(Game.this, Results.class);
+                        intent.putExtra("correct", String.valueOf(foundList.size()));
+                        intent.putExtra("incorrect", String.valueOf(incorrect));
+                        intent.putExtra("total", String.valueOf(foundList.size() + incorrect));
+                        startActivity(intent);
+                    }
+
+                }.start();
                 foundList.add(word);
                 foundAdapter.notifyDataSetChanged();
                 lv.setSelection(foundAdapter.getCount() - 1);
+                //}
+                //else {
+                //incorrect++;
+                //time -5 seconds? (5000ms)
+                //}
             }
         }
     }
 
     public class foundClick implements View.OnClickListener {
 
-        public void onClick(View view){
+        public void onClick(View view) {
             int i = view.getId();
             Button b = (Button) view.findViewById(i);
             clickedButtons.remove(b);
-            LinearLayout wc = (LinearLayout)findViewById(R.id.word_container);
-            LinearLayout fc = (LinearLayout)findViewById(R.id.found_container);
+            LinearLayout wc = (LinearLayout) findViewById(R.id.word_container);
+            LinearLayout fc = (LinearLayout) findViewById(R.id.found_container);
             fc.removeView(b);
             wc.addView(b);
             b.setOnClickListener(new letterClick());
--- app/src/main/java/com/urch/dance/anagram/Results.java
+++ app/src/main/java/com/urch/dance/anagram/Results.java
@@ -1,7 +1,11 @@
 package com.urch.dance.anagram;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Results extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class Results extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        Intent intent = getIntent();
+        //Bundle extras = intent.getExtras();
+        TextView correct = (TextView) findViewById(R.id.correctNo);
+        TextView incorrect = (TextView) findViewById(R.id.incorrectNo);
+        TextView total = (TextView) findViewById(R.id.totalNo);
+        //if(extras != null){
+        String i = intent.getStringExtra("correct");//}
+        correct.setText(intent.getStringExtra("correct"));;//String.valueOf(extras.getInt("correct")));
+        incorrect.setText(intent.getStringExtra("incorrect"));;
+        total.setText(intent.getStringExtra("total"));;
+    }
+
+    public void title(View view) {
+        Intent intent = new Intent(this, Title.class);
+        startActivity(intent);
     }
 }
